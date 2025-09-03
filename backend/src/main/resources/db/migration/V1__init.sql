cddf0fe5c35e41f69690c8a6b926056563b31faa
Sarath-2803 <sarathkrishna357@gmail.com>
2025-09-03 20:17:19 +0530

    updated db config

--- backend/src/main/resources/db/migration/V1__init.sql
+++ backend/src/main/resources/db/migration/V1__init.sql
@@ -3,7 +3,8 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(100) NOT NULL UNIQUE,
     email VARCHAR(50) NOT NULL UNIQUE,
     phone VARCHAR(20) NOT NULL,
-    role VARCHAR(50),
+    type VARCHAR(50) NOT NULL CHECk (type in ('GUEST','NON-GUEST')),
+    loyalty INTEGER DEFAULT 0,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -12,42 +13,52 @@ CREATE TABLE IF NOT EXISTS rooms (
     room_number VARCHAR(50) NOT NULL UNIQUE,
     type VARCHAR(50) NOT NULL,
     price VARCHAR(100) NOT NULL,
-    status VARCHAR(50),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    available BOOLEAN DEFAULT TRUE
 );
 
 CREATE TABLE IF NOT EXISTS amenity (
     id SERIAL PRIMARY KEY,
     type VARCHAR(50) NOT NULL,
-    status VARCHAR(100) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    available BOOLEAN DEFAULT TRUE
 );
 
-CREATE TABLE IF NOT EXISTS booking (
+CREATE TABLE IF NOT EXISTS room_booking (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER,
-    room_id INTEGER,
-    amenity_id INTEGER,
+    user_id INTEGER NOT NULL,
+    room_id INTEGER NOT NULL,
     check_in DATE NOT NULL,
     check_out DATE NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT booking_user_fk FOREIGN KEY (user_id)
+
+    CONSTRAINT booking_room_user_fk FOREIGN KEY (user_id)
         REFERENCES users(id)
         ON DELETE CASCADE,
-    CONSTRAINT booking_room_fk FOREIGN KEY (room_id)
+    CONSTRAINT booking_room_room_fk FOREIGN KEY (room_id)
         REFERENCES rooms(id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS amenity_booking(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    amenity_id INTEGER NOT NULL,
+    start_time TIMESTAMP NOT NULL,
+    end_time TIMESTAMP NOT NULL,
+
+    CONSTRAINT booking_amenity_user_fk FOREIGN KEY (user_id)
+        REFERENCES users(id)
         ON DELETE CASCADE,
-    CONSTRAINT booking_amenity_fk FOREIGN KEY (amenity_id)
+    CONSTRAINT booking_amenity_amenity_fk FOREIGN KEY (amenity_id)
         REFERENCES amenity(id)
         ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS payment (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER,
-    method VARCHAR(255) NOT NULL,
-    status VARCHAR(50),
+    user_id INTEGER NOT NULL,
+    amount INTEGER NOT NULL,
+    status VARCHAR(20) DEFAULT 'PENDING',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
     CONSTRAINT payment_user_fk FOREIGN KEY (user_id)
         REFERENCES users(id)
         ON DELETE CASCADE
