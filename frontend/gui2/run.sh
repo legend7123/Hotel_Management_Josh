9e29a6d00d27832cabd60339affd227ee638f513
Johan <jv.1406k@gmail.com>
2025-09-06 16:00:54 +0000

    Added BookingFrame

--- frontend/gui2/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-# Compile
-javac --module-path /usr/share/openjfx/lib --add-modules javafx.controls,javafx.fxml */*.java
-
-
-# Run
-java --module-path /usr/share/openjfx/lib --add-modules javafx.controls,javafx.fxml main.Main
