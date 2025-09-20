f3e95925c4262101dbff3a830af269b637841c4f
Sarath-2803 <sarathkrishna357@gmail.com>
2025-09-20 22:50:58 +0530

    added route to update loyalty by id

--- backend/src/main/java/hotel/controller/UserController.java
+++ backend/src/main/java/hotel/controller/UserController.java
@@ -35,6 +35,7 @@ public class UserController{
 	public void registerRoutes(HttpServer server){
 		server.createContext("/user",this::handleUserPath);
 		server.createContext("/user/",this::handleUserIdPath);
+//		server.createContext("/user/loyalty",this::handleUserLoyalty);
 	}
 
 	/*====================================================*/
@@ -48,6 +49,8 @@ public class UserController{
 				     break;
 			case "POST" : createUser(exchange);
 				      break;
+			case "PUT" : updateUser(exchange);
+				     break;
 
 		}
 	}
@@ -58,11 +61,61 @@ public class UserController{
 		String path = exchange.getRequestURI().getPath(); // /user/1
     		String[] parts = path.split("/");
     		Long id = Long.parseLong(parts[2]);
-
+/*
 		if(method!=null && id!=0){
 			switch(method.toUpperCase()){
 				case "GET" : displayUserById(exchange,id);
+					     break;
+				//case "PUT" : updateUserLoyalty(exchange,id);
+				//	     break
 			}
+		}
+*/
+		if(parts.length == 3 && method.toUpperCase().equals("GET")){
+			displayUserById(exchange,id);
+		}else if(parts.length == 4 && method.toUpperCase().equals("PUT")){
+			handleUserLoyalty(exchange,parts);
+		}
+	}
+
+	public void handleUserLoyalty(HttpExchange exchange,String[] parts) throws IOException{
+	//	String method = exchange.getRequestMethod();
+	//	String path = exchange.getRequestURI().getPath();
+	//	String[] parts = path.split("/");
+
+	//	if(parts.length<3){
+	//		ResponseDTO response = new ResponseDTO(400,"Invalid URL");
+	//		ResponseUtils.sendResponse(exchange,response);
+	//		System.out.println("Invalid url...");
+	//		return;
+	//	}
+		
+		Long id = Long.parseLong(parts[2]);
+
+		// Read query parameter
+    		String query = exchange.getRequestURI().getQuery();  // loyalty=100
+    		Map<String, String> params = parseQuery(query);
+    		if (!params.containsKey("loyalty")) {
+        		ResponseDTO response = new ResponseDTO(400, "Missing loyalty parameter");
+        		ResponseUtils.sendResponse(exchange, response);
+        		return;
+    		}
+
+		Double loyalty = Double.parseDouble(params.get("loyalty"));
+
+		try(Connection conn = Utils.getConnection()){
+			User user = userService.updateLoyalty(id,loyalty,conn);
+			ResponseDTO response = new ResponseDTO(200,user);
+			ResponseUtils.sendResponse(exchange,response);
+			System.out.println("User loyalty updated");
+		} catch(SQLException e){
+    			System.out.println("error :" + e.getMessage());
+    			ResponseDTO response = new ResponseDTO(500,e.getMessage());
+			ResponseUtils.sendResponse(exchange,response);
+		} catch(Exception e){
+    			System.out.println("error :" + e.getMessage());
+    			ResponseDTO response = new ResponseDTO(500,e.getMessage());
+			ResponseUtils.sendResponse(exchange,response);
 		}
 	}
 
@@ -87,25 +140,6 @@ public class UserController{
     			ResponseDTO response = new ResponseDTO(500,e.getMessage());
 			ResponseUtils.sendResponse(exchange,response);
 		}
-	}
-
-	//GET user/id
-	public void displayUserById(HttpExchange exchange,Long id) throws IOException{
-			System.out.println("Received request: " + exchange.getRequestMethod() + " " + exchange.getRequestURI());
-
-			try(Connection conn = Utils.getConnection()){
-				User user = userService.getById(id,conn);
-				ResponseDTO response = new ResponseDTO(200,user);
-				ResponseUtils.sendResponse(exchange,response);
-			} catch(SQLException e){
-				System.out.println("error :" + e.getMessage());
-	    			ResponseDTO response = new ResponseDTO(500,e.getMessage());
-				ResponseUtils.sendResponse(exchange,response);
-			} catch(Exception e){
-				System.out.println("error :" + e.getMessage());
-    				ResponseDTO response = new ResponseDTO(500,e.getMessage());
-				ResponseUtils.sendResponse(exchange,response);
-			}
 	}
 
 
@@ -154,6 +188,79 @@ public class UserController{
 		}
 	}
 
+	
+	// /user - PUT
+	public void updateUser(HttpExchange exchange) throws IOException{
+		System.out.println("Received request: " + exchange.getRequestMethod() + " " + exchange.getRequestURI());
+		
+		//Read body
+		InputStreamReader isr = new InputStreamReader(exchange.getRequestBody(),"utf-8");
+		BufferedReader br = new BufferedReader(isr);
+		StringBuilder buf = new StringBuilder();
+		String line;
+		while((line = br.readLine()) != null){
+			buf.append(line);
+		}
+		br.close();
+		String requestBody = buf.toString();
+		System.out.println("Request body: " + requestBody);
+
+		//JSON parsing
+		String idstr = extractJsonValue(requestBody,"id");
+		String username = extractJsonValue(requestBody,"username");
+		String email = extractJsonValue(requestBody,"email");
+		String phone = extractJsonValue(requestBody,"phone");
+		String loyaltystr = extractJsonValue(requestBody,"loyalty");	
+
+		Long id = Long.parseLong(idstr);
+		Double loyalty = Double.parseDouble(loyaltystr);
+
+		if(id == null || username == null || email == null || phone == null || loyalty == null){
+			ResponseDTO response = new ResponseDTO(500,"Missing parameters");
+			ResponseUtils.sendResponse(exchange,response);
+			System.out.println("params : " + idstr + " " + username + " " + email + " " + phone + " " + loyaltystr);
+			System.out.println("Error updating user : Missing parameter...");
+			return;
+		}
+	
+
+		User user = new User(id,username,email,phone,loyalty);
+
+		try(Connection conn = Utils.getConnection()){
+			User updatedUser = userService.updateUser(user,conn);
+			ResponseDTO response = new ResponseDTO(200,updatedUser);
+			ResponseUtils.sendResponse(exchange,response);
+			System.out.println("User updated successfully...");
+		} catch(SQLException e){
+			ResponseDTO response = new ResponseDTO(500,e.getMessage());
+			ResponseUtils.sendResponse(exchange,response);
+			System.out.println("Error updating user : " + e.getMessage());
+		} catch(RuntimeException e){
+			ResponseDTO response = new ResponseDTO(500,e.getMessage());
+			ResponseUtils.sendResponse(exchange,response);
+			System.out.println("Error updating user : " + e.getMessage());
+		}
+	}
+
+
+	// /user/id - GET
+	public void displayUserById(HttpExchange exchange,Long id) throws IOException{
+			System.out.println("Received request: " + exchange.getRequestMethod() + " " + exchange.getRequestURI());
+
+			try(Connection conn = Utils.getConnection()){
+				User user = userService.getById(id,conn);
+				ResponseDTO response = new ResponseDTO(200,user);
+				ResponseUtils.sendResponse(exchange,response);
+			} catch(SQLException e){
+				System.out.println("error :" + e.getMessage());
+	    			ResponseDTO response = new ResponseDTO(500,e.getMessage());
+				ResponseUtils.sendResponse(exchange,response);
+			} catch(RuntimeException e){
+				System.out.println("error :" + e.getMessage());
+    				ResponseDTO response = new ResponseDTO(500,e.getMessage());
+				ResponseUtils.sendResponse(exchange,response);
+			}
+	}
 
 
 /*	private vo
@@ -176,7 +283,7 @@ public class UserController{
     		sendResponse(exchange, json);
 	}
 */
-	private String extractJsonValue(String json, String key){
+/*	private String extractJsonValue(String json, String key){
     		String pattern = "\"" + key + "\"\\s*:\\s*\"([^\"]*)\"";
     		java.util.regex.Pattern r = java.util.regex.Pattern.compile(pattern);
     		java.util.regex.Matcher m = r.matcher(json);
@@ -185,6 +292,32 @@ public class UserController{
     		}
     		return null;
 	}
+*/
+	private String extractJsonValue(String json, String key){
+		// Match "key": "value" OR "key": value (number, boolean)
+    		String pattern = "\"" + key + "\"\\s*:\\s*\"?([^\"]+?)\"?(,|})";
+    		java.util.regex.Pattern r = java.util.regex.Pattern.compile(pattern);
+    		java.util.regex.Matcher m = r.matcher(json);
+    		if (m.find()){
+        		return m.group(1).trim();
+    		}
+    		return null;
+	}
+
+	public static Map<String, String> parseQuery(String query) {
+    		Map<String, String> map = new java.util.HashMap<>();
+    if (query == null || query.isEmpty()) return map;
+
+    String[] pairs = query.split("&");
+    for (String pair : pairs) {
+        String[] kv = pair.split("=");
+        if (kv.length == 2) {
+            map.put(kv[0], kv[1]);
+        }
+    }
+    return map;
+}
+
 
 
 }
