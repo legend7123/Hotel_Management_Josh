c52b58ea99485538446a8acd837fa79b391b6a68
legend7123 <josh60282@gmail.com>
2025-10-04 10:53:45 +0530

    Migration

--- backend/src/main/java/util/MigrationRunner.java
+++ backend/src/main/java/util/MigrationRunner.java
@@ -9,39 +9,39 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class MigrationRunner{
-	private static final String MIG_DIR="/home/sarath/Projects/oop/Java-HMS/backend/src/main/resources/db/migrations/";
+    private static final String MIG_DIR="src/main/resources/db/migrations/"; // FIXED PATH
 
-	public static void main(String args[]){
-		try(Connection conn = Utils.getConnection()){
-			if(conn!=null && !conn.isClosed()){
-				System.out.println("DB connected successfully!");
-				runMigrations(conn);
-			}
-			else{
-				System.out.println("DB connection closed...");
-			}
-		}
-		catch(SQLException e){
-			System.out.println(e.getMessage());
-			throw new RuntimeException(e);
-		}
-		catch(Exception e){
-			throw new RuntimeException(e);
-		}
-	}
+    public static void main(String args[]){
+        try(Connection conn = Utils.getConnection()){
+            if(conn!=null && !conn.isClosed()){
+                System.out.println("DB connected successfully!");
+                runMigrations(conn);
+            }
+            else{
+                System.out.println("DB connection closed...");
+            }
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+        catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
 
-	public static void runMigrations(Connection conn) throws Exception{
-		List<Path> sqlFiles = Files.list(Paths.get(MIG_DIR))
-			.filter(f -> f.toString().endsWith(".sql"))
-			.sorted().toList();
+    public static void runMigrations(Connection conn) throws Exception{
+        List<Path> sqlFiles = Files.list(Paths.get(MIG_DIR))
+            .filter(f -> f.toString().endsWith(".sql"))
+            .sorted().toList();
 
-        	for (Path path : sqlFiles) {
-            		System.out.println("Running migration: " + path.getFileName());
-            		String sql = Files.readString(path);
-            		try (Statement stmt = conn.createStatement()) {
-                		stmt.execute(sql);
-            		}
-        	}
-        	System.out.println("All migrations executed successfully!");
-	}
+        for (Path path : sqlFiles) {
+            System.out.println("Running migration: " + path.getFileName());
+            String sql = Files.readString(path);
+            try (Statement stmt = conn.createStatement()) {
+                stmt.execute(sql);
+            }
+        }
+        System.out.println("All migrations executed successfully!");
+    }
 }
